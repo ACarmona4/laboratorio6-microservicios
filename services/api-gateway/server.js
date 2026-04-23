ca3181b8159742cc20c243a9deb3f6b0fb3002f0
Sebastian Acevedo <sacevedog2@eafit.edu.co>
2026-04-22 22:28:35 -0500

    migracion de api-gateway con node.js a imagen nginx liviana

--- services/api-gateway/server.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import app from './app.js';
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`API Gateway REST service running on port ${PORT}`);
-});
--- services/api-gateway/src/routes/reviewsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createReview, getReviewsByBookId } from '../controllers/reviewsController.js';
-
-const router = express.Router();
-
-router.get('/book/:bookId', getReviewsByBookId);
-router.post('/', createReview);
-
-export default router;
