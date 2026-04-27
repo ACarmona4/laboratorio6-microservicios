b93a0de299be2ee5bebc1750d3ff2bf45e524a4f
ACarmona4 <acarmonas@eafit.edu.co>
2026-04-27 18:27:05 -0500

    Despliegue

--- db-init/init.sql
+++ db-init/init.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS orders (
 -- Insert dummy data for books
 INSERT INTO books (id, name, author, description, price, count_in_stock, image)
 VALUES
-    ('1', 'El principito', 'Antoine de Saint-Exupéry', 'Un cuento poético que viene acompañado de ilustraciones hechas con acuarelas por el mismo Saint-Exupéry.', 45000, 10, 'http://localhost:5001/images/principito.jpg'),
-    ('2', 'Cien años de soledad', 'Gabriel García Márquez', 'Obra maestra de la literatura hispanoamericana y universal, así como una de las obras más traducidas y leídas en español.', 65000, 5, 'http://localhost:5001/images/ciena.jpg'),
-    ('3', '1984', 'George Orwell', 'Novela política de ficción distópica, escrita por George Orwell entre 1947 y 1948 y publicada el 8 de junio de 1949.', 55000, 8, 'http://localhost:5001/images/1984.jpg')
+    ('1', 'El principito', 'Antoine de Saint-Exupéry', 'Un cuento poético que viene acompañado de ilustraciones hechas con acuarelas por el mismo Saint-Exupéry.', 45000, 10, '/images/libro1.jpg'),
+    ('2', 'Cien años de soledad', 'Gabriel García Márquez', 'Obra maestra de la literatura hispanoamericana y universal, así como una de las obras más traducidas y leídas en español.', 65000, 5, '/images/libro2.jpg'),
+    ('3', '1984', 'George Orwell', 'Novela política de ficción distópica, escrita por George Orwell entre 1947 y 1948 y publicada el 8 de junio de 1949.', 55000, 8, '/images/libro3.jpg')
 ON CONFLICT (id) DO NOTHING;
